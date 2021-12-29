913e3406f773101c45c9581cab4e768bbd60b046
Desdavido <72236341+Desdavido@users.noreply.github.com>
2021-12-28 21:16:34 -0500

    website and hiragana

---
+++ website/hiragana/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.hiragana, name='hiragana'),
+    path('<character>/', views.quiz_character, name='quiz_character'),
+    path('<character>/<submission>', views.check_answer, name='check_answer'),
+]
--- website/hiragana/views.py
+++ website/hiragana/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import redirect
+import random
+hiragana_table = {
+    "あ": "a",
+    "い": "i",
+    "う": "u",
+    "え": "e",
+    "お": "o",
+    "か": "ka"
+}
 
-# Create your views here.
+
+def hiragana(request):
+    character, _ = random.choice(list(hiragana_table.items()))
+    return redirect('/hiragana/' + character)
+
+
+def quiz_character(request, character):
+    template = loader.get_template('website/hiragana/quiz_character.html')
+
+    options = hiragana_table.values()
+
+    context = {
+        'character': character,
+        'options': options,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def check_answer(request, character, submission):
+    # show a new page with my results + have a link to the /hiragana/
+    template = loader.get_template('website/hiragana/quiz_result.html')
+
+    if hiragana_table[character] == submission:
+        result = "You are correct"
+    else:
+        result = "You are wrong"
+
+    context = {
+        'result': result,
+    }
+    return HttpResponse(template.render(context, request))
+
